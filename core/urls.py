aedfb2af6f78176f006c2d38ab11d7742d470b05
Martin Irarrazabal <m.irarrazabalj@gmail.com>
2024-06-27 22:50:17 -0400

    listo inicio

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path , include
+from .views import home , products , exit , register
+urlpatterns = [
+    path('',home, name = 'home'),
+    path('products/',products, name = 'products'),
+    path('products/',products, name = 'products'),
+    path('logout/',exit, name = 'exit'),
+    path('register/', register, name='register'),
+
+
+
+
+]
